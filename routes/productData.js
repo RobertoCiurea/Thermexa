cd6452ce85e29b066bd72e5c67b97dc82051fe27
RobertoCiurea <robertociureaionut@gmail.com>
2023-04-24 15:48:39 +0300

    added delete item from shopping cart option

--- routes/productData.js
+++ routes/productData.js
@@ -2,16 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 //POST data from product items
-
-let dataArr = [];
-
+let items = [];
+let itemId = 1;
 router.post("/shopping-cart", async (req, res) => {
   let data = await req.body.data;
-  await dataArr.push(data);
-  console.log(dataArr);
+  await items.push({ ...data, id: itemId++ });
+  console.log(items);
 });
 router.get("/shopping-cart", async (req, res) => {
-  await res.render("shopping-cart", { products: dataArr });
+  await res.render("shopping-cart", { products: items });
 });
 
+router.post("/shopping-cart/delete", (req, res) => {
+  const itemId = Number(req.body.itemId);
+  console.log(itemId);
+  items = items.filter((item) => item.id !== itemId);
+  res.redirect("/shopping-cart");
+});
 module.exports = router;
