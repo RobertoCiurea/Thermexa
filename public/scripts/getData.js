f3d19920a7637101357be3c523e1caf297e4dcd0
RobertoCiurea <robertociureaionut@gmail.com>
2023-05-26 20:17:44 +0300

    added notification pop-up

--- public/scripts/getData.js
+++ public/scripts/getData.js
@@ -13,9 +13,21 @@ buttons.forEach((button) => {
   const data = { description, price };
   //send elements to node server
   button.addEventListener("click", () => {
+    //send data to node
     const xml = new XMLHttpRequest();
     xml.open("POST", "/shopping-cart");
     xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xml.send(JSON.stringify({ data }));
+    const interval = setInterval(removeNotification, 1500);
+
+    const notification = document.querySelector(".notification");
+    notification.classList.add("active");
+
+    function removeNotification() {
+      notification.classList.remove("active");
+      clearInterval(interval);
+    }
   });
+
+  //add notification
 });
